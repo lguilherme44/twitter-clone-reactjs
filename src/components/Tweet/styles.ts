442397f6e0680a524fdfa8f06302b86e24813465
Guilherme Lellis <lguilherme44@gmail.com>
2020-07-18 00:32:17 -0300

    iniciando o component menu bar

--- src/components/Tweet/styles.ts
+++ src/components/Tweet/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { Chat, Rocketseat, Favorite } from '../../styles/Icons';
 
@@ -85,18 +85,101 @@ export const Header = styled.div`
   }
 `;
 
-export const Dot = styled.div``;
+export const Dot = styled.div`
+  background: var(--gray);
+  width: 2px;
+  height: 2px;
+  margin: 0 10px;
+`;
 
-export const Description = styled.div``;
+export const Description = styled.p`
+  font-size: 14px;
+  margin-top: 4px;
+`;
 
-export const ImageContent = styled.div``;
+export const ImageContent = styled.div`
+  margin-top: 12px;
+  width: 100%;
+  height: min(285px, max(175px, 41vw));
 
-export const Icons = styled.div``;
+  background: var(--outline);
+  border-radius: 14px;
 
-export const Status = styled.div``;
+  cursor: pointer;
 
-export const CommentIcon = styled.div``;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
 
-export const RetweetIcon = styled.div``;
+export const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: 11px auto 0;
+  width: 100%; /** Mobile */
 
-export const LikeIcon = styled.div``;
+  @media (min-width: 330px) {
+    width: 63%; /** Desktop */
+  }
+
+  > div {
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+`;
+
+export const Status = styled.div`
+  display: flex;
+  align-items: center;
+
+  font-size: 14px;
+
+  > svg {
+    margin-right: 5px;
+  }
+
+  &:nth-child(1) {
+    &,
+    > svg path {
+      color: var(--gray);
+    }
+  }
+
+  &:nth-child(2) {
+    color: var(--retweet);
+
+    > svg path {
+      fill: var(--retweet);
+    }
+  }
+
+  &:nth-child(3) {
+    color: var(--like);
+
+    > svg {
+      fill: var(--like);
+    }
+  }
+`;
+
+const iconCSS = css`
+  width: 19px;
+  height: 19px;
+`;
+
+export const CommentIcon = styled(Chat)`
+  ${iconCSS}
+`;
+
+export const RetweetIcon = styled(Rocketseat)`
+  ${iconCSS}
+`;
+
+export const LikeIcon = styled(Favorite)`
+  ${iconCSS}
+`;
